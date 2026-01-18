197ab24f51886427f998ed3e60fd06a1a8e0d165
Corbin <csplatti@gmail.com>
2026-01-18 00:57:22 -0800

    change back to correct port

--- rd-flg/app/add-tos/page.tsx
+++ rd-flg/app/add-tos/page.tsx
@@ -32,7 +32,7 @@ export default function AddTos() {
   const [error, setError] = useState('');
   const router = useRouter();
 
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:3000';
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://localhost:6767';
 
   useEffect(() => {
     const unsubscribeAuth = onAuthStateChanged(auth, (currentUser) => {
